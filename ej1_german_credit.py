95dd408dde45164d2dd51e51a92ab30167c3b7bd
Paula Domingues <pdomingues@itba.edu.ar>
2023-09-08 13:34:25 -0300

    feat: discretizing non discrete variables

--- ej1_german_credit.py
+++ ej1_german_credit.py
@@ -3,12 +3,21 @@ import numpy as np
 from utils.data_split import k_fold_split
 
 
+def discretize_variables(df: pd.DataFrame, var: str, bins_amount: int):
+    min_val = df[var].min()
+    max_val = df[var].max()
+    bin_width = (max_val - min_val) / bins_amount
+    bin_edges = [min_val + i * bin_width for i in range(bins_amount + 1)]
+    bin_labels = [i for i in range(bins_amount)]
+    df[var] = pd.cut(df[var], bins=bin_edges, labels=bin_labels, include_lowest=True)
+
+
 def run_ej1():
     df = pd.read_csv("./data/german_credit.csv", delimiter=',', encoding='utf-8')
-    random_generator = np.random.default_rng(seed=42)
-    k = 3
+    discretize_variables(df, 'Credit Amount', 5)
+    discretize_variables(df, 'Duration of Credit (month)', 4)
+    discretize_variables(df, 'Age (years)', 3)
 
-    train_df, test_df = k_fold_split(df, k, random_generator)
+    train_df, test_df = k_fold_split(df, k=3)
     print(train_df)
     print(test_df)
-
