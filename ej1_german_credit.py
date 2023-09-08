dbf0241ca08b04c183ef0b6d734348c19a0fe8d8
Paula Domingues <pdomingues@itba.edu.ar>
2023-09-08 11:56:52 -0300

    feat: reading and splitting ej1 data

---
+++ ej1_german_credit.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from utils.data_split import k_fold_split
+
+
+def run():
+    df = pd.read_csv("./data/german_credit.csv", delimiter=',', encoding='utf-8')
+    random_generator = np.random.default_rng(seed=42)
+    k = 3
+
+    train_df, test_df = k_fold_split(df, k, random_generator)
+
+    print(train_df)
+    print(test_df)
+
