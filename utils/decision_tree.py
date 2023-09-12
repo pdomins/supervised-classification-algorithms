a423ffc56892a3955bf310b079d1019b7f81f12a
Luciana Diaz Kralj <lucidiaz@itba.edu.ar>
2023-09-12 22:29:28 +0000

    feat: added wip on DecisionTree class

---
+++ utils/decision_tree.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+class DecisionTree:
+
+    def __init__(self):
+        # TODO
+        pass
+        
+    def evaluate(self, test_sample : pd.Series):
+        # TODO
+        pass
