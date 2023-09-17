7990900d382ed5eba840a3c7c6e8a49c508c65bb
Paula Domingues <pdomingues@itba.edu.ar>
2023-09-17 10:18:26 -0300

    fix: style

--- ej2_reviews_sentiment.py
+++ ej2_reviews_sentiment.py
@@ -26,11 +26,21 @@ def normalize_column(column):
     return (column - min_val) / (max_val - min_val)
 
 
-# def fill_missing_vals(df: pd.DataFrame):
-#
+def normalize_df(df: pd.DataFrame):
+    df['wordcount'] = normalize_column(df['wordcount'])
+    df['titleSentiment'] = df['titleSentiment'].apply(lambda x: 1 if x == 'positive' else 0)
+    df['sentimentValue'] = normalize_column(df['sentimentValue'])
+    return df
+
+
+def handle_n_a(df: pd.DataFrame, drop: bool = True):
+    if drop:
+        df = df.dropna(subset=["wordcount", "titleSentiment", "textSentiment", "Star Rating", "sentimentValue"])
+    return df
+
 
 def print_results(k, conf_matrix, per_label_conf_matrix):
-    print(f"Results for k = {k}:")
+    print(f"\nResults for k = {k}:")
     print("Confusion Matrix:")
     print(conf_matrix)
 
@@ -38,50 +48,49 @@ def print_results(k, conf_matrix, per_label_conf_matrix):
     # print(per_label_conf_matrix)
 
     metrics_result = metrics(per_label_conf_matrix)
-    print("Precision:")
-    print(
-        f"positive: {metrics_result['positive']['Precision']}, negative: {metrics_result['negative']['Precision']}")
+    for key, value in metrics_result.items():
+        precision = value['Precision']
+        print(f'Key {key}: Precision = {precision}')
 
 
 def run_ej2():
     df = pd.read_csv("./data/reviews_sentiment.csv", delimiter=';', encoding='utf-8')
     print(f"Original Length: {len(df)}")
+
     df = df.drop_duplicates()
     print(f"Length with no duplicates: {len(df)}")
-    # comment one of  the following lines
-    df = df.dropna(subset=["wordcount", "titleSentiment", "textSentiment", "Star Rating", "sentimentValue"])
-    # fill_missing_vals(df)
+
+    df = handle_n_a(df, drop=True)
     print("--------------------")
     title_sentiment_stats(df)
     mean_words_amount_per_rating(df)
     df.drop(columns=["Review Title", "Review Text", "textSentiment"], inplace=True)
-    # normalize values
-    df['wordcount'] = normalize_column(df['wordcount'])
-    # df['titleSentiment'] = df['titleSentiment'].apply(lambda x: '1' if x == 'positive' else '0')
-    # df['titleSentiment'] = normalize_column(df['titleSentiment'])
-    df['sentimentValue'] = normalize_column(df['sentimentValue'])
 
+    df = normalize_df(df)
+    print(df)
     # k split
     k = 3
     train_df, test_df = k_fold_split(df, k)
 
     # kNN
-    class_labels = np.array(['positive', 'negative'])
+    to_predict = "Star Rating"
+    # to_predict = "titleSentiment"
+    class_labels = np.array(df[to_predict].unique())
     start = 1
     stop = 42
     step = 2
 
     print("kNN")
     for k in range(start, stop, step):
-        res = kNN(train_df, test_df, "titleSentiment", k, False)
-        conf_matrix = calculate_confusion_matrix(class_labels, res['predictions'], res['titleSentiment'])
+        res = kNN(train_df, test_df, to_predict, k, False)
+        conf_matrix = calculate_confusion_matrix(class_labels, res['predictions'], res[to_predict])
         per_label_conf_matrix = calculate_per_label_confusion_matrix_from_confusion_matrix(conf_matrix)
         print_results(k, conf_matrix, per_label_conf_matrix)
 
     print("--------------------")
     print("weighted kNN")
     for k in range(start, stop, step):
-        res = kNN(train_df, test_df, "titleSentiment", k, True)
-        conf_matrix = calculate_confusion_matrix(class_labels, res['predictions'], res['titleSentiment'])
+        res = kNN(train_df, test_df, to_predict, k, True)
+        conf_matrix = calculate_confusion_matrix(class_labels, res['predictions'], res[to_predict])
         per_label_conf_matrix = calculate_per_label_confusion_matrix_from_confusion_matrix(conf_matrix)
         print_results(k, conf_matrix, per_label_conf_matrix)
